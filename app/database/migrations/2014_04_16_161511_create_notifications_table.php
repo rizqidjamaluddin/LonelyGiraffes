f2e655d9069aa9739694664f64f66692e8d30a9a
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-01 13:00:58 +0700

    Add softDeletes to notifications
    Mark read notifications as soft deleted, and regularly trim old read notifications.

--- app/database/migrations/2014_04_16_161511_create_notifications_table.php
+++ app/database/migrations/2014_04_16_161511_create_notifications_table.php
@@ -19,6 +19,7 @@ class CreateNotificationsTable extends Migration {
             $table->string('service');
             $table->string('message');
             $table->string('metadata');
+            $table->softDeletes();
 			$table->timestamps();
 		});
 	}
