5a8825199f25e6f3f2087057e75f14c409ea24f2
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-02 13:42:50 +0700

    Revert bootstrap.sh to commit b415f5dc
    Just to keep the develop branch stable.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 
-echo ">>> Starting Lonely Giraffes install scripts"
-
-echo ">>> Setting dependencies"
+echo ">>> Starting Install Script"
+ 
+# Update
+sudo apt-get update
+ 
+# Install MySQL without prompt
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+ 
+echo ">>> Installing Base Items"
+ 
+# Install base items
+sudo apt-get install -y vim tmux curl wget build-essential python-software-properties redis-server
+ 
+echo ">>> Adding PPA's and Installing Server Items"
+ 
+# Add repo for PHP5 and nodejs
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y vim tmux curl wget  build-essential chrpath libssl-dev python-software-properties redis-server libfontconfig1-dev git-core php5 apache2 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-mcrypt php5-xdebug mysql-server nodejs
 
-# xdebug Config
+# Update again, probably for good measure
+sudo apt-get update
+ 
+# Install the Rest
+sudo apt-get install -y git-core php5 apache2 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-mcrypt php5-xdebug mysql-server nodejs
+ 
 echo ">>> Configuring Server"
+ 
+# xdebug Config
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
 xdebug.scream=0
 xdebug.cli_color=1
@@ -19,7 +36,6 @@ xdebug.show_local_vars=1
 EOF
  
 # Apache Config
-echo ">>> Configuring Apache"
 sudo a2enmod rewrite
 curl -L https://gist.github.com/fideloper/2710970/raw/vhost.sh > vhost
 sudo chmod guo+x vhost
@@ -27,17 +43,18 @@ sudo mv vhost /usr/local/bin
 sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 sudo vhost -d /vagrant/public -s 192.168.33.10.xip.io
 
+ 
 # PHP Config
-echo ">>> Configuring PHP"
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 # Remove some disabled functions for Boris / artisan tinker
 grep -E '^disable_functions' /etc/php5/cli/php.ini | sed -r 's/pcntl_(signal|fork|waitpid|signal_dispatch),//g' > /etc/php5/cli/conf.d/99-boris.ini
 
 sudo service apache2 restart
-  
-# Install Composer
+ 
 echo ">>> Installing Composer"
+ 
+# Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 # Add global phpunit - do the next 2 lines if it says phpunit isn't installed
@@ -45,13 +62,16 @@ sudo composer global require 'phpunit/phpunit=3.7.*'
 printf 'PATH="~/.composer/vendor/bin:$PATH"' > ~/.bash_profile
 source ~/.bash_profile
 
-# Start Redis
+# Start redis
 echo ">>> Starting Redis"
 sudo redis-server /etc/redis/redis.conf
 
 # Create main db
-echo ">>> Create main database for project"
 mysql -u root -proot -e 'create database lg'
+
+# Update dependencies
+cd /vagrant
+composer update -vvv
 
 # Node already installed; do other node installs here
 echo ">>> Installing Grunt command line interface"
@@ -62,7 +82,7 @@ echo ">>> Installing project dependencies via Node"
 npm install
 
 # Run necessary things for grunt
-echo ">>> Running Grunt for development"
+echo ">>> Running Grunt for development machine"
 grunt development
 
 # Run unit tests
