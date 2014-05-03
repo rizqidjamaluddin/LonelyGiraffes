4f4c22e0e7547e9d654f8c7438d52937c6f20645
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-03 09:18:29 +0700

    Add InvalidCreationException

---
+++ app/Giraffe/Exceptions/InvalidCreationException.php
@@ -0,0 +1,8 @@
+<?php  namespace Giraffe\Exceptions; 
+
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class InvalidCreationException extends BadRequestHttpException
+{
+
+} 
