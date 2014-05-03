97734fb8a2ce0d96f7554947c26e18006e4bc7fa
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-03 09:48:21 +0700

    Change NotImplementedException to use HTTP 724

--- app/Giraffe/Exceptions/NotImplementedException.php
+++ app/Giraffe/Exceptions/NotImplementedException.php
@@ -1,5 +1,27 @@
 <?php  namespace Giraffe\Exceptions; 
-class NotImplementedException extends \Exception
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+/**
+ * Uses 724 This Line Should Be Unreachable
+ *
+ * Leaving it as an easter egg if anyone finds an unimplemented feature.
+ */
+class NotImplementedException extends \Exception implements HttpExceptionInterface
 {
 
-} 
+    public function __construct()
+    {
+        parent::__construct('This line should be unreachable');
+    }
+
+
+    public function getStatusCode()
+    {
+        return 724;
+    }
+
+    public function getHeaders()
+    {
+        return [];
+    }
+}
