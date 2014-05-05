e231c744bd14a96988452c60e125c34aec16b08c
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-05 11:37:08 +0700

    Add Gatekeeper class

---
+++ app/Giraffe/Helpers/Rights/Gatekeeper.php
@@ -0,0 +1,92 @@
+<?php  namespace Giraffe\Helpers\Rights;
+
+/**
+ * Class Gatekeeper
+ *
+ * Basic use:
+ * $gatekeeper->iAm($user)->andMayI('edit', 'posts')->please();
+ *
+ * Stateful:
+ * $gatekeeper->iAm($user);
+ * $gatekeeper->mayI('edit', 'posts');
+ *
+ * Authenticating:
+ * $gatekeeper->iAm($generic_user_identifier)->pleaseLogMeIn();
+ * $gatekeeper->iAm($generic_user_identifier)->andMyPasswordIs($password)->pleaseLogMeIn();
+ * $gatekeeper->myTokenIs($identification_token);
+ *
+ * Planned (assuming user already set using iAm()):
+ * $gatekeeper->put($mod_to_be)->into('moderators')->please();
+ * $gatekeeper->remove($inactive_person)->from('moderators')->please();
+ * $gatekeeper->allow($some_dude)->to('edit', 'posts')->please();
+ * $gatekeeper->ban($poor_guy)->from('edit', 'posts')->please();
+ *
+ * In the future:
+ * $gatekeeper->ban($poor_guy)->from('edit', 'posts')->for('1 month')->and()->removeThem()->from('moderators')->please();
+ *
+ * Nouns may be plural or singular.
+ *
+ * @package Giraffe\Helpers\Rights
+ */
+class Gatekeeper
+{
+
+    /**
+     * @var array
+     */
+    protected $query = [];
+
+    protected $request = null;
+
+    protected $authenticated = false;
+    protected $authentication = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function iAm($userIdentifier)
+    {
+
+    }
+
+    public function myTokenIs($token)
+    {
+
+    }
+
+    public function myTokenSays($token){ return $this->myTokenIs($token); }
+
+    public function andMyPasswordIs($password)
+    {
+
+    }
+
+
+
+
+    public function mayI($verb, $noun)
+    {
+        
+    }
+
+    public function andMayI($verb, $noun)
+    {
+
+    }
+
+    /**
+     * Execute command and clear the query details.
+     */
+    public function please()
+    {
+
+    }
+
+    public function pleaseLogMeIn()
+    {
+
+    }
+    
+}
