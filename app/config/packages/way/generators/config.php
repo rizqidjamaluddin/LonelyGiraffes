b5f7c007869a6fcbe1ae223c2945928366955162
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-07 20:35:18 +0700

    Fix config for way generator templates

--- app/config/packages/way/generators/config.php
+++ app/config/packages/way/generators/config.php
@@ -8,19 +8,19 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'model_template_path' => '/vagrant/laravel/app/templates/model.txt',
+    'model_template_path' => '/vagrant/app/templates/model.txt',
 
-    'scaffold_model_template_path' => '/vagrant/laravel/app/templates/scaffolding/model.txt',
+    'scaffold_model_template_path' => '/vagrant/app/templates/scaffolding/model.txt',
 
-    'controller_template_path' => '/vagrant/laravel/app/templates/controller.txt',
+    'controller_template_path' => '/vagrant/app/templates/controller.txt',
 
-    'scaffold_controller_template_path' => '/vagrant/laravel/app/templates/scaffolding/controller.txt',
+    'scaffold_controller_template_path' => '/vagrant/app/templates/scaffolding/controller.txt',
 
-    'migration_template_path' => '/vagrant/laravel/app/templates/migration.txt',
+    'migration_template_path' => '/vagrant/app/templates/migration.txt',
 
-    'seed_template_path' => '/vagrant/laravel/app/templates/seed.txt',
+    'seed_template_path' => '/vagrant/app/templates/seed.txt',
 
-    'view_template_path' => '/vagrant/laravel/app/templates/view.txt',
+    'view_template_path' => '/vagrant/app/templates/view.txt',
 
 
     /*
