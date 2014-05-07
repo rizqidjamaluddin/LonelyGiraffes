53f284a563f42fbb7215005969c17c906bbcb9c5
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-07 17:27:20 +0700

    Add typehints on GatekeeperProvider

--- app/Giraffe/Helpers/Rights/GatekeeperProvider.php
+++ app/Giraffe/Helpers/Rights/GatekeeperProvider.php
@@ -1,7 +1,23 @@
 <?php  namespace Giraffe\Helpers\Rights;
 
+use stdClass;
+
 interface GatekeeperProvider
 {
+    /**
+     * Obtain a user model. Should fetch a proper user model from the persistence layer.
+     *
+     * @param $user mixed Arbitrary representation of a user.
+     * @return stdClass
+     */
     public function getUserModel($user);
+
+    /**
+     * @param $user stdClass
+     * @param $verb string
+     * @param $noun string
+     *
+     * @return bool
+     */
     public function checkIfUserMay($user, $verb, $noun);
 } 
