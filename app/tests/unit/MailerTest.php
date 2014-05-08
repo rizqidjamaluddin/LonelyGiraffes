0eaef1174a70b260262f07bc2c3ff12070fc4266
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-08 15:50:45 +0700

    Fix unlabeled test in MailerTest

--- app/tests/unit/MailerTest.php
+++ app/tests/unit/MailerTest.php
@@ -2,8 +2,11 @@
 
 class MailerTest extends TestCase
 {
+    /**
+     * @test
+     */
     public function it_can_send_a_notification_email()
     {
         $mailer = App::make('Giraffe\Helpers\Mailer\Mailer');
     }
-} 
+}
