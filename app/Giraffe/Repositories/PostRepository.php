d725c286735a6a0cd3385f733a4a4aeaf8bf4923
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-09 21:12:38 +0700

    Add PostRepository

---
+++ app/Giraffe/Repositories/PostRepository.php
@@ -0,0 +1,18 @@
+<?php  namespace Giraffe\Repositories; 
+
+use Giraffe\Models\PostModel;
+
+class PostRepository extends BaseEloquentRepository
+{
+
+    /**
+     * @var \Giraffe\Models\PostModel
+     */
+    private $postModel;
+
+    public function __construct(PostModel $postModel)
+    {
+        $this->postModel = $postModel;
+    }
+
+} 
