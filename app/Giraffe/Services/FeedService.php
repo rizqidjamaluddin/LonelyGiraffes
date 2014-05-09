9f254ef1737d2a96d58653fc4e4af0f515df972b
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-10 01:40:35 +0700

    Fill in FeedService::createPost, create test

--- app/Giraffe/Services/FeedService.php
+++ app/Giraffe/Services/FeedService.php
@@ -1,14 +1,89 @@
-<?php  namespace Giraffe\Services; 
-class FeedService 
+<?php  namespace Giraffe\Services;
+
+use Eloquent;
+use Giraffe\Helpers\Parser\Parser;
+use Giraffe\Helpers\Rights\Gatekeeper;
+use Giraffe\Models\PostModel;
+use Giraffe\Repositories\PostRepository;
+use Giraffe\Repositories\ShoutRepository;
+use Giraffe\Repositories\UserRepository;
+
+class FeedService
 {
+
+    /**
+     * @var \Giraffe\Helpers\Rights\Gatekeeper
+     */
+    private $gatekeeper;
+    /**
+     * @var \Giraffe\Repositories\PostRepository
+     */
+    private $postRepository;
+    /**
+     * @var \Giraffe\Repositories\UserRepository
+     */
+    private $userRepository;
+    /**
+     * @var \Giraffe\Repositories\ShoutRepository
+     */
+    private $shoutRepository;
+    /**
+     * @var \Giraffe\Helpers\Parser\Parser
+     */
+    private $parser;
+
+    public function __construct(
+        Gatekeeper $gatekeeper,
+        Parser $parser,
+        PostRepository $postRepository,
+        UserRepository $userRepository,
+        ShoutRepository $shoutRepository
+        )
+    {
+        $this->gatekeeper = $gatekeeper;
+        $this->postRepository = $postRepository;
+        $this->userRepository = $userRepository;
+        $this->shoutRepository = $shoutRepository;
+        $this->parser = $parser;
+    }
+
     public function addCommentOnPost($user, $post, $comment)
     {
 
     }
 
+    /**
+     * Create a new post.
+     *
+     * Expected metadata:
+     * type: Type of post, defaults to shout if not set or unknown
+     *
+     * @param        $user
+     * @param string $post
+     * @param array  $metadata
+     *
+     * @return PostModel
+     */
     public function createPost($user, $post, array $metadata)
     {
+        $this->gatekeeper->mayI('create', 'post');
 
+        $user = $this->userRepository->get($user);
+        $type = array_key_exists('type', $metadata) ? $metadata['type'] : null;
+        switch ($type) {
+            default: {
+                $htmlBody = $this->parser->parseComment($post);
+                $postable = $this->shoutRepository->create([
+                        'user_id'   => $user->id,
+                        'body'      => $post,
+                        'html_body' => $htmlBody
+                    ]);
+                break;
+            }
+        }
+
+        $post = $this->postRepository->createWithPostable(['user_id' => $user->id], $postable);
+        return $post;
     }
 
     public function getBuddyFeed($user)
@@ -24,5 +99,15 @@ class FeedService
     public function getLocalFeed($user)
     {
 
+    }
+
+    /**
+     * @param $post
+     *
+     * @return Eloquent
+     */
+    public function getPost($post)
+    {
+        return $this->postRepository->get($post);
     }
 } 
--- app/tests/unit/FeedServiceTest.php
+++ app/tests/unit/FeedServiceTest.php
@@ -2,11 +2,53 @@
 
 class FeedServiceTest extends TestCase
 {
+
+    const POST_REPOSITORY = 'Giraffe\Repositories\PostRepository';
+
+    const TEST = 'Giraffe\Services\FeedService';
+
+    const SHOUT_REPOSITORY = 'Giraffe\Repositories\ShoutRepository';
+
+    const USER_REPOSITORY = 'Giraffe\Repositories\UserRepository';
+
+    public function disarm()
+    {
+        $gatekeeper = App::make('Giraffe\Helpers\Rights\Gatekeeper');
+        $gatekeeper->disarm();
+    }
+
     /**
      * @test
      */
-    public function it()
+    public function it_can_create_a_shout_post()
     {
-        $feed = App::make('Giraffe\Services\FeedService');
+        $this->disarm();
+
+        $userRepository = Mockery::mock(self::USER_REPOSITORY);
+        $userRepository->shouldReceive('get')
+            ->with('1')
+            ->andReturn(json_decode("{'id': 1}"));
+        App::instance(self::USER_REPOSITORY, $userRepository);
+
+        $shoutRepository = Mockery::mock(self::SHOUT_REPOSITORY);
+        $shoutRepository->shouldReceive('create')
+            ->with(['user_id' => 1, 'body' => 'Lorem ipsum', 'html_body' => '<p>Lorem ipsum</p>'])
+            ->andReturn(json_decode('{"id": 1}'));
+        App::instance(self::SHOUT_REPOSITORY, $shoutRepository);
+
+        $postRepository = Mockery::mock(self::POST_REPOSITORY);
+        $postRepository->shouldReceive('createWithPostable')
+            ->with(['user_id' => 1], Mockery::any())
+            ->andReturn(json_decode('{"id": 2, "postable": {"body": "Lorem ipsum"}}'));
+        $postRepository->shouldReceive('get')
+            ->withAnyArgs()
+            ->andReturn(json_decode('{"id": 2, "postable": {"body": "Lorem ipsum"}}'));
+        App::instance(self::POST_REPOSITORY, $postRepository);
+
+        $feed = App::make(self::TEST);
+        $post = $feed->createPost('1', 'Lorem ipsum', []);
+        $postModel = $feed->getPost($post);
+
+        $this->assertEquals($postModel->postable->body, 'Lorem ipsum');
     }
 }
