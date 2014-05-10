6b08a86569ab8c5115c9bd96fafa39904ad8887f
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-05-10 20:20:53 +0700

    Remove FeedService::createPost
    Clients should directly create Events, Shouts, etc and they will generate their own posts.

--- app/tests/unit/FeedServiceTest.php
+++ app/tests/unit/FeedServiceTest.php
@@ -13,42 +13,5 @@ class FeedServiceTest extends TestCase
         $gatekeeper = App::make('Giraffe\Helpers\Rights\Gatekeeper');
         $gatekeeper->disarm();
     }
-
-    /**
-     * @test
-     */
-    public function it_can_create_a_shout_post()
-    {
-        $this->disarm();
-
-        $userRepository = Mockery::mock(self::USER_REPOSITORY);
-        $userRepository->shouldReceive('get')
-            ->with('1')
-            ->andReturn(json_decode('{"id": 1, "country": "United States"}'));
-        App::instance(self::USER_REPOSITORY, $userRepository);
-
-        $shoutRepository = Mockery::mock(self::SHOUT_REPOSITORY);
-        $shoutRepository->shouldReceive('create')
-            ->with(['user_id' => 1, 'body' => 'Lorem ipsum', 'html_body' => '<p>Lorem ipsum</p>'])
-            ->andReturn(json_decode('{"id": 1}'));
-        App::instance(self::SHOUT_REPOSITORY, $shoutRepository);
-
-        $postRepository = Mockery::mock(self::POST_REPOSITORY);
-        $postRepository->shouldReceive('createWithPostable')
-            ->withAnyArgs()
-            ->andReturn(json_decode('{"id": 2, "postable": {"body": "Lorem ipsum"}}'));
-        $postRepository->shouldReceive('get')
-            ->withAnyArgs()
-            ->andReturn(json_decode('{"id": 2, "postable": {"body": "Lorem ipsum"}, "country": "United States"}'));
-        App::instance(self::POST_REPOSITORY, $postRepository);
-
-        $feed = App::make(self::TEST);
-        $post = $feed->createPost('1', 'Lorem ipsum', []);
-        $postModel = $feed->getPost($post);
-
-        // assert post properly saved
-        $this->assertEquals($postModel->postable->body, 'Lorem ipsum');
-        // assert post takes user location if not explicitly set
-        $this->assertEquals($postModel->country, 'United States');
-    }
+    
 }
