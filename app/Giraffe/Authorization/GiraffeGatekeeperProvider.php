83ff7b6f95ef75459f22daa917128d9ffc09594d
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-05 21:42:45 +0700

    Fleshing out GiraffeGatekeeperProvider

--- app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
+++ app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
@@ -1,5 +1,6 @@
 <?php  namespace Giraffe\Authorization;
 
+use Giraffe\Common\ConfigurationException;
 use Giraffe\Users\UserRepository;
 
 class GiraffeGatekeeperProvider implements GatekeeperProvider
@@ -8,6 +9,11 @@ class GiraffeGatekeeperProvider implements GatekeeperProvider
      * @var \Giraffe\Users\UserModel
      */
     protected $userModel;
+
+    /**
+     * @var array
+     */
+    protected $permissions;
 
     /**
      * @var \Giraffe\Users\UserRepository
@@ -35,12 +41,36 @@ class GiraffeGatekeeperProvider implements GatekeeperProvider
 
     public function checkIfUserMay($user, $verb, $noun, $model = null)
     {
-        $this->getUserModel($user);
-        return false;
+        $role = $this->getUserModel($user)->role;
+        return $this->resolve($role, $verb, $noun, $model);
     }
 
     public function checkIfGuestMay($verb, $noun, $model = null)
     {
-        return false;
+        return $this->resolve('guest', $verb, $noun, $model);
+    }
+
+    protected function resolve($role, $verb, $noun, $model = null)
+    {
+        $permissions = $this->permissions ?: $this->permissions = $this->permissionsLookup->getGroupPermissions();
+
+        if ($model) {
+
+        } else {
+            // look for user role in permissions lookup
+            if (!array_key_exists($role, $permissions)) {
+                throw new ConfigurationException("User role does not exist in permissions table.");
+            }
+
+            // look for list of user's "self" permissions
+            $permissionList = $permissions[$role]['self'];
+            if (!array_key_exists($noun, $permissionList)) {
+                // noun not found at all, assume access denied
+                return false;
+            }
+
+            // look if verb is listed in array of permitted verbs
+            return in_array($verb, $permissionList[$noun]);
+        }
     }
 }
