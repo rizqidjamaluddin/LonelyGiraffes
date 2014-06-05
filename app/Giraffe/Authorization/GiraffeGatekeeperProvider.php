77473798c45af19c13af5fd097333916b43b027b
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-05 09:56:48 +0700

    Add lookup to gatekeeper provider

--- app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
+++ app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
@@ -4,30 +4,43 @@ use Giraffe\Users\UserRepository;
 
 class GiraffeGatekeeperProvider implements GatekeeperProvider
 {
+    /**
+     * @var \Giraffe\Users\UserModel
+     */
+    protected $userModel;
 
     /**
      * @var \Giraffe\Users\UserRepository
      */
     private $userRepository;
+    /**
+     * @var GiraffePermissionsLookup
+     */
+    private $permissionsLookup;
 
-    public function __construct(UserRepository $userRepository)
+    /**
+     * @param UserRepository           $userRepository
+     * @param GiraffePermissionsLookup $permissionsLookup
+     */
+    public function __construct(UserRepository $userRepository, GiraffePermissionsLookup $permissionsLookup)
     {
         $this->userRepository = $userRepository;
+        $this->permissionsLookup = $permissionsLookup;
     }
 
     public function getUserModel($user)
     {
-        return $this->userRepository->get($user);
+        return $this->userModel ?: $this->userModel = $this->userRepository->get($user);
     }
 
     public function checkIfUserMay($user, $verb, $noun, $model = null)
     {
+        $this->getUserModel($user);
         return false;
     }
 
-    public function checkIfGuestMay($verb, $noun)
+    public function checkIfGuestMay($verb, $noun, $model = null)
     {
-        // TODO: Implement checkIfGuestMay() method.
         return false;
     }
 }
