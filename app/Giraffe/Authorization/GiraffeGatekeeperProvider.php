e3edb60dad4ea010ed999d6616b4317a6cc00b67
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-06 06:50:29 +0700

    Add permissions merging between self/global permissions on a model
    GiraffeGatekeeperProvider should be done for the most part.

--- app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
+++ app/Giraffe/Authorization/GiraffeGatekeeperProvider.php
@@ -1,7 +1,9 @@
 <?php  namespace Giraffe\Authorization;
 
 use Giraffe\Common\ConfigurationException;
+use Giraffe\Users\UserModel;
 use Giraffe\Users\UserRepository;
+use stdClass;
 
 class GiraffeGatekeeperProvider implements GatekeeperProvider
 {
@@ -36,13 +38,13 @@ class GiraffeGatekeeperProvider implements GatekeeperProvider
 
     public function getUserModel($user)
     {
-        return $this->userModel ?: $this->userModel = $this->userRepository->get($user);
+        return $this->userRepository->get($user);
     }
 
     public function checkIfUserMay($user, $verb, $noun, $model = null)
     {
         $role = $this->getUserModel($user)->role;
-        return $this->resolve($role, $verb, $noun, $model);
+        return $this->resolve($role, $verb, $noun, $model, $this->getUserModel($user));
     }
 
     public function checkIfGuestMay($verb, $noun, $model = null)
@@ -50,27 +52,56 @@ class GiraffeGatekeeperProvider implements GatekeeperProvider
         return $this->resolve('guest', $verb, $noun, $model);
     }
 
-    protected function resolve($role, $verb, $noun, $model = null)
+    protected function resolve($role, $verb, $noun, $model = null, $user = null)
     {
-        $permissions = $this->permissions ?: $this->permissions = $this->permissionsLookup->getGroupPermissions();
+        $permissions = $this->loadGroupPermissions();
 
-        if ($model) {
+        // normalize casing
+        $noun = strtolower($noun);
+        $verb = strtolower($verb);
 
-        } else {
-            // look for user role in permissions lookup
-            if (!array_key_exists($role, $permissions)) {
-                throw new ConfigurationException("User role does not exist in permissions table.");
-            }
-
-            // look for list of user's "self" permissions
-            $permissionList = $permissions[$role]['self'];
-            if (!array_key_exists($noun, $permissionList)) {
-                // noun not found at all, assume access denied
-                return false;
-            }
-
-            // look if verb is listed in array of permitted verbs
-            return in_array($verb, $permissionList[$noun]);
+        // look for user role in permissions lookup
+        if (!array_key_exists($role, $permissions)) {
+            throw new ConfigurationException("User role does not exist in permissions table.");
         }
+
+        // look for list of user's "self" permissions
+        $selfPermissionList = $permissions[$role]['self'];
+        $globalPermissionList = $permissions[$role]['global'];
+        if (!(array_key_exists($noun, $selfPermissionList) || array_key_exists($noun, $globalPermissionList))) {
+            // noun not found at all, assume access denied
+            return false;
+        }
+
+        // compile list of permitted verbs for this particular noun
+        $selfPermittedVerbs = array_key_exists($noun, $selfPermissionList) ? $selfPermissionList[$noun] : [];
+        $globalPermittedVerbs = array_key_exists($noun, $globalPermissionList) ? $globalPermissionList[$noun] : [];
+
+        // it doesn't make any sense to check for a model's permission if the user isn't given as well;
+        // default to non-model authorization if user is not provided.
+        if ($model && $user) {
+            if (!$model instanceof ProtectedResource) {
+                throw new ConfigurationException('Model ' . get_class($model) . ' must implement Gatekeeper\ProtectedResource');
+            }
+
+            // globally permitted verbs for this noun override self permissions
+            if (in_array($verb, $globalPermittedVerbs)) {
+                return true;
+            }
+
+            $owner = $model->getOwner();
+            return $owner->id === $user->id;
+        } else {
+            return in_array($verb, array_merge_recursive($selfPermittedVerbs, $globalPermittedVerbs));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function loadGroupPermissions()
+    {
+        $permissions = $this->permissions ? : $this->permissions = $this->permissionsLookup->getGroupPermissions();
+        return $permissions;
     }
 }
