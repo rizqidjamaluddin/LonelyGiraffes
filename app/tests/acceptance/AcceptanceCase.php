af2a7416054b0c78c3d3960085af121eed24afc0
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-07 07:11:57 +0700

    Add AcceptanceCase abstract class, create test to attempt deletion as a non-admin

---
+++ app/tests/acceptance/AcceptanceCase.php
@@ -0,0 +1,36 @@
+<?php
+
+use Giraffe\Authorization\Gatekeeper;
+
+abstract class AcceptanceCase extends TestCase
+{
+
+    /**
+     * @var Gatekeeper
+     */
+    protected $gatekeeper;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->gatekeeper = App::make('Giraffe\Authorization\Gatekeeper');
+    }
+
+    protected function createAdministratorAccount()
+    {
+        $userRepository = App::make('Giraffe\Users\UserRepository');
+        $admin = $userRepository->create(
+            [
+                'firstname' => 'Admin',
+                'lastname' => 'Member',
+                'gender' => 'M',
+                'email' => 'admin@lonelygiraffes.net',
+                'password' => 'password',
+                'role' => 'admin',
+                'hash' => Str::random(20),
+            ]
+        );
+        return $admin;
+    }
+
+} 
