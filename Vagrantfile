46239b696200bc121ebe5f356b31d2890e2a46a7
Matt Lindahl <mlindahl@Matts-MacBook-Pro.local>
2014-06-08 12:33:41 -0600

    adding new CentOS 6.5 Vagrant files

--- Vagrantfile
+++ Vagrantfile
@@ -6,8 +6,9 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.box = "CentOS6.5"
+  #config.vm.box_url = "https://github.com/2creatives/vagrant-centos/releases/download/v6.5.3/centos65-x86_64-20140116.box"
+  config.vm.provision :shell, path: "bootstrap.sh"
 
   # NFS folder sharing
   config.vm.network :private_network, ip: "192.168.33.10"
@@ -29,21 +30,5 @@ config.vm.provider "virtualbox" do |v|
 end
 
 
-  # Share an additional folder to the guest VM. The first argument is
-  # the path on the host to the actual folder. The second argument is
-  # the path on the guest to mount the folder. And the optional third
-  # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
 
-  # Provider-specific configuration so you can fine-tune various
-  # backing providers for Vagrant. These expose provider-specific options.
-  # Example for VirtualBox:
-  #
-  # config.vm.provider :virtualbox do |vb|
-  #   # Don't boot with headless mode
-  #   vb.gui = true
-  #
-  #   # Use VBoxManage to customize the VM. For example to change memory:
-  #   vb.customize ["modifyvm", :id, "--memory", "1024"]
-  # end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,92 +1,40 @@
 #!/usr/bin/env bash
 
-echo ">>> Starting Install Script"
- 
-# Update
-sudo apt-get update
- 
-# Install MySQL without prompt
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
- 
-# Install base items
-echo ">>> Installing Base Items"
-sudo apt-get install -y vim tmux curl wget build-essential python-software-properties redis-server
-  
-# Add repo for PHP5 and nodejs
-echo ">>> Adding PPA's and Installing Server Items"
-sudo add-apt-repository -y ppa:ondrej/php5
-sudo add-apt-repository -y ppa:chris-lea/node.js
+#####################
+#Install base things#
+#####################
 
-# Update again, probably for good measure
-sudo apt-get update
- 
-# Install the Rest
-sudo apt-get install -y git-core php5 apache2 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-mcrypt php5-xdebug mysql-server nodejs
-  
-# xdebug Config
-echo ">>> Configuring Server"
-cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
-xdebug.scream=0
-xdebug.cli_color=1
-xdebug.show_local_vars=1
-EOF
- 
-# Apache Config
-sudo a2enmod rewrite
-curl -L https://gist.github.com/fideloper/2710970/raw/vhost.sh > vhost
-sudo chmod guo+x vhost
-sudo mv vhost /usr/local/bin
-sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-sudo vhost -d /vagrant/public -s 192.168.33.10.xip.io
+yum -y install gcc make perl wget nano 
 
- 
-# PHP Config
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-# Remove some disabled functions for Boris / artisan tinker
-grep -E '^disable_functions' /etc/php5/cli/php.ini | sed -r 's/pcntl_(signal|fork|waitpid|signal_dispatch),//g' > /etc/php5/cli/conf.d/99-boris.ini
+##################################
+#Install PHP 5.5 and dependencies#
+##################################
+#rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
+#yum -y install php55w php55w-opcache php55w-xml php55w-mcrypt php55w-sqlite php55w-pdo php55w-pear php55w-mbstring php55w-gd
 
-sudo service apache2 restart
-  
-# Composer
-echo ">>> Installing Composer"
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-# Add global phpunit - do the next 2 lines if it says phpunit isn't installed
-sudo composer global require 'phpunit/phpunit=3.7.*'
-printf 'PATH="~/.composer/vendor/bin:$PATH"' > ~/.bash_profile
-source ~/.bash_profile
+###########################
+#Install nginx and php-fpm#
+###########################
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
-# Start redis
-echo ">>> Starting Redis"
-sudo redis-server /etc/redis/redis.conf
+#touch /etc/yum.repos.d/nginx.repo
 
-# Create main db
-echo ">>> Create LG database"
-mysql -u root -proot -e 'create database lg'
+#echo "[nginx]
+#name=nginx repo
+#baseurl=http://nginx.org/packages/centos/6/x86_64/
+#gpgcheck=0
+#enabled=1" > /etc/yum.repos.d/nginx.repo
 
-# Update dependencies
-echo ">>> Running Composer"
-cd /vagrant
-composer update -vvv
+yum -y --enablerepo=remi,remi-php55 install nginx php-fpm
 
-# Run unit tests
-echo ">>> Running PHPUnit"
-phpunit
+yum -y --enablerepo=remi,remi-php55 install php php-opcache php-xml php-mcrypt php-pdo php-pear php-mbstring php-gd
 
-# Node already installed; do other node installs here
-echo ">>> Installing Grunt command line interface"
-sudo npm install -g grunt-cli
+###################################
+#Start nginx and php-fpm processes#
+###################################
+/etc/init.d/nginx start
+/etc/init.d/php-fpm start
+chkconfig nginx on
+chkconfig php-fpm on
 
-# Install node dependencies
-echo ">>> Installing project dependencies via Node"
-npm install
-
-# Run necessary things for grunt
-echo ">>> Running Grunt for development machine"
-cd /vagrant/
-grunt development
-
-# if laravel refuses to start properly, adjust the permissions afterwards:
-# chmod -R o+w /vagrant/app/storage
