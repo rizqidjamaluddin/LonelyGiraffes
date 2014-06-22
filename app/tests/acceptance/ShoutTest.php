d11b7ced52e827e850658af3e1798919a092526a
Sethen Maleno <sethenm@gmail.com>
2014-06-22 19:11:51 -0400

    Added more acceptance tests.

--- app/tests/acceptance/ShoutTest.php
+++ app/tests/acceptance/ShoutTest.php
@@ -51,7 +51,7 @@ class ShoutTest extends AcceptanceCase
      * @test
      * @depends users_can_post_shouts
      */
-    public function users_can_get_all_shouts_from_another_user()
+    public function users_can_get_all_of_their_shouts()
     {
         $model = $this->toJson($this->call('POST', '/api/users/', $this->genericUser));
         $shoutBodies = [
@@ -75,6 +75,62 @@ class ShoutTest extends AcceptanceCase
         $this->assertEquals($shoutBodies[1], $getShouts->shouts[1]->body);
         $this->assertEquals($shoutBodies[2], $getShouts->shouts[2]->body);
         $this->assertEquals($shoutBodies[3], $getShouts->shouts[3]->body);
+    }
+
+    /**
+     * @test
+     * @depends users_can_post_shouts
+     */
+    public function users_can_get_all_of_another_users_shouts()
+    {
+        $model = $this->toJson($this->call('POST', '/api/users/', $this->genericUser));
+        $anotherModel = $this->toJson($this->call('POST', '/api/users/', $this->anotherGenericUser));
+        $shoutBodies = [
+            'This is a shout!',
+            'This is also a shout!',
+            'Another shout I am posting!',
+            'Yay for shouts!'
+        ];
+        $moreShoutBodies = [
+            'This is a shout from another user!',
+            'This is also a shout from another user!',
+            'Another shout I am posting from another user!',
+            'Yay for shouts from another user!'
+        ];
+
+        $this->asUser($model->users[0]->hash);
+
+        for ($i = 0; $i < count($shoutBodies); $i++) {
+            $this->call('POST', '/api/shouts', [
+                    'body' => $shoutBodies[$i]
+                ]
+            );
+        }
+
+        $this->asUser($anotherModel->users[0]->hash);
+
+        $getShouts = $this->toJson($this->call('GET', '/api/shouts/user/' . $model->users[0]->hash));
+        $this->assertResponseStatus(200);
+        $this->assertEquals($shoutBodies[0], $getShouts->shouts[0]->body);
+        $this->assertEquals($shoutBodies[1], $getShouts->shouts[1]->body);
+        $this->assertEquals($shoutBodies[2], $getShouts->shouts[2]->body);
+        $this->assertEquals($shoutBodies[3], $getShouts->shouts[3]->body);
+
+        for ($i = 0; $i < count($moreShoutBodies); $i++) {
+            $this->call('POST', '/api/shouts', [
+                    'body' => $moreShoutBodies[$i]
+                ]
+            );
+        }
+
+        $this->asUser($model->users[0]->hash);
+
+        $getShouts = $this->toJson($this->call('GET', '/api/shouts/user/' . $anotherModel->users[0]->hash));
+        $this->assertResponseStatus(200);
+        $this->assertEquals($moreShoutBodies[0], $getShouts->shouts[0]->body);
+        $this->assertEquals($moreShoutBodies[1], $getShouts->shouts[1]->body);
+        $this->assertEquals($moreShoutBodies[2], $getShouts->shouts[2]->body);
+        $this->assertEquals($moreShoutBodies[3], $getShouts->shouts[3]->body);
     }
 
     /**
