54e09a793e4ef1677c7741f9f71a22ebffeefbe5
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-22 21:13:51 +0700

    Fix undefined property in ShoutTest

--- app/tests/acceptance/ShoutTest.php
+++ app/tests/acceptance/ShoutTest.php
@@ -11,7 +11,7 @@ class ShoutTest extends AcceptanceCase
     {
         $text = 'This is a shout!';
         $model = $this->toJson($this->call('POST', '/api/users/', $this->genericUser));
-        $this->asUser($model->data->hash);
+        $this->asUser($model->users[0]->hash);
 
         $response = $this->toJson($this->call('POST', '/api/shouts', [
                 'body' => $text
