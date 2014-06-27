f572b157dca9fe32c9fec9cb79e55f2bcd0b4219
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-27 07:55:44 +0700

    Refactor GeonameLocationProvider

--- app/Giraffe/Geolocation/Providers/GeonameLocationProvider.php
+++ app/Giraffe/Geolocation/Providers/GeonameLocationProvider.php
@@ -18,14 +18,61 @@ class GeonameLocationProvider implements LocationProvider
     public function search($hint)
     {
         // look up by city
-        $cities = new Collection(DB::table(self::CITY_TABLE)->where('city', 'LIKE', '%' . $hint . '%')->get());
+        $cities = $this->searchForCities($hint);
 
-        /*
-            Look up by state - return highest pop city.
-            There isn't a single consistent way to make this a join so only one result is returned each
-            so we'll just do a naive multi-query lookup. Caching will help.
-        */
+        // Look up by state - return highest pop city.
+        $stateCities = $this->searchViaState($hint);
 
+        // look up by country - return highest pop city
+        $countryCities = $this->searchViaCountry($hint);
+
+        // merge and transform
+        $cities = $cities->merge($stateCities, $countryCities);
+        $results = $this->transformToLocations($cities);
+
+        return $results;
+
+    }
+
+    /**
+     * @param $city
+     * @return string
+     */
+    protected function getCompositeIdentifier($city)
+    {
+        return $city->country_code . '.' . $city->state_code . '.' . $city->city;
+    }
+
+    /**
+     * @param $hint
+     * @return Collection
+     */
+    protected function searchViaCountry($hint)
+    {
+        /** @var Array $countries */
+        $countries = DB::table('lookup_countries')->where('name', 'LIKE', '%' . $hint . '%')->get();
+        $countryCities = new Collection();
+        foreach ($countries as $country) {
+            $countryCities->push(
+                DB::table(self::CITY_TABLE)
+                  ->where('country_code', $country->code)
+                  ->orderBy('population', 'desc')
+                  ->first()
+            );
+        }
+        $countryCities->sortBy('population', SORT_NUMERIC, true);
+        return $countryCities;
+    }
+
+    /**
+     * There isn't a single consistent way to make this a join so only one result is returned each
+     * so we'll just do a naive multi-query lookup. Caching will help.
+     *
+     * @param $hint
+     * @return Collection
+     */
+    protected function searchViaState($hint)
+    {
         /** @var Array $states */
         $states = DB::table('lookup_geoname_states')->where('name', 'LIKE', '%' . $hint . '%')
                     ->take(self::STATE_SEARCH_CAP)
@@ -40,51 +87,44 @@ class GeonameLocationProvider implements LocationProvider
             );
         }
         $stateCities->sortBy('population', SORT_NUMERIC, true);
+        return $stateCities;
+    }
 
+    /**
+     * @param $hint
+     * @return Collection
+     */
+    protected function searchForCities($hint)
+    {
+        $cities = new Collection(DB::table(self::CITY_TABLE)->where('city', 'LIKE', '%' . $hint . '%')->get());
+        return $cities;
+    }
 
-        // look up by country - return highest pop city
-        /** @var Array $countries */
-        $countries = DB::table('lookup_countries')->where('name', 'LIKE', '%' . $hint . '%')->get();
-        $countryCities = new Collection();
-        foreach ($countries as $country) {
-            $countryCities->push(
-                DB::table(self::CITY_TABLE)
-                  ->where('country_code', $country->code)
-                  ->orderBy('population', 'desc')
-                  ->first()
-            );
-        }
-        $countryCities->sortBy('population', SORT_NUMERIC, true);
-
-        // filter duplicates, convert to Location entities
-        $cities = $cities->merge($stateCities, $countryCities);
-
+    /**
+     * @param $cities
+     * @return array
+     */
+    protected function transformToLocations($cities)
+    {
         // registry contains a list of cities in the result set to prevent duplicates
         $registry = [];
         $results = [];
         foreach ($cities as $city) {
+
+            // skip duplicates
             if (in_array($this->getCompositeIdentifier($city), $registry)) {
                 continue;
             }
 
+            // convert and fill it data
             $place = Location::makeFromCity($city->city, $city->state, $city->country);
             $place->provideCoordinates($city->lat, $city->long);
             $results[] = $place;
 
+            // register identifier for duplicate checks
             $registry[] = $this->getCompositeIdentifier($city);
         }
-
         return $results;
-
-    }
-
-    /**
-     * @param $city
-     * @return string
-     */
-    protected function getCompositeIdentifier($city)
-    {
-        return $city->country_code . '.' . $city->state_code . '.' . $city->city;
     }
 
 }
