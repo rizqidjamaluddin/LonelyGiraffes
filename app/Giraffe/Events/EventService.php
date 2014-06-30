4cae3eb054813637d7bf79a2da31cc0189e76875
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-06-30 21:41:47 +0700

    Add EventTransformer and flesh out EventService::create, get

--- app/Giraffe/Events/EventService.php
+++ app/Giraffe/Events/EventService.php
@@ -1,6 +1,9 @@
 <?php  namespace Giraffe\Events;
 
 use Giraffe\Common\Service;
+use Giraffe\Parser\Parser;
+use Giraffe\Users\UserRepository;
+use Giraffe\Users\UserService;
 use Str;
 
 class EventService extends Service
@@ -22,21 +25,42 @@ class EventService extends Service
      */
     private $requestRepository;
 
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+    /**
+     * @var Parser
+     */
+    private $parser;
+
     public function __construct(
         EventRepository $eventRepository,
         EventAttendeeRepository $attendeeRepository,
         EventInvitationRepository $invitationRepository,
-        EventRequestRepository $requestRepository
+        EventRequestRepository $requestRepository,
+        UserRepository $userRepository,
+        Parser $parser
     ) {
+        parent::__construct();
+
         $this->eventRepository = $eventRepository;
         $this->attendeeRepository = $attendeeRepository;
         $this->invitationRepository = $invitationRepository;
         $this->requestRepository = $requestRepository;
+        $this->userRepository = $userRepository;
+        $this->parser = $parser;
     }
 
-    public function createEvent($data)
+    public function createEvent($user, $data)
     {
+        $this->gatekeeper->mayI('create', 'event')->please();
+
+        $data = array_only($data, ['name', 'body', 'url', 'location', 'city', 'state', 'country', 'timestamp']);
         $data['hash'] = Str::random(32);
+        $data['html_body'] = $this->parser->parseComment($data['body']);
+        $user = $this->userRepository->getByHash($user);
+        $data['user_id'] = $user->id;
         return $this->eventRepository->create($data);
     }
 
@@ -52,9 +76,16 @@ class EventService extends Service
         return $event;
     }
 
-    public function updateEvent($hash, $attributes)
+    public function updateEvent($hash, $data)
     {
-        return $this->eventRepository->update($hash, $attributes);
+        $event = $this->eventRepository->getByHash($hash);
+        $this->gatekeeper->mayI('update', $hash)->please();
+
+        $data = array_only($data, ['name', 'body', 'url', 'location', 'city', 'state', 'country', 'timestamp']);
+        if (array_key_exists('body', $data)) {
+            $data['html_body'] = $this->parser->parseComment($data['body']);
+        }
+        return $this->eventRepository->update($hash, $data);
     }
 
     public function createRequest($event, $requestingUser)
