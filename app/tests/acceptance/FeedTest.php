88afa60f4efe247b00db5745c6a8f23cc5435b59
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-03 22:42:32 +0700

    Create first FeedTest

--- app/tests/acceptance/FeedTest.php
+++ app/tests/acceptance/FeedTest.php
@@ -1,11 +1,29 @@
 <?php
 
-abstract class FeedTest extends AcceptanceCase
+class FeedTest extends AcceptanceCase
 {
 
-    public function it_can_fetch_the_latest_few_posts()
+    /**
+     * @test
+     */
+    public function it_can_fetch_the_latest_few_posts_in_the_global_feed()
     {
+        // add some test shouts
+        $this->registerAndLoginAsMario();
+        for ($i = 0; $i < 12; $i++) {
+            $this->call('POST', '/api/shouts', ['body' => 'Hello world, I am a shout!']);
+        }
 
+        // fetch them
+        $results = $this->toJson($this->call('GET', '/api/posts'));
+
+        $results = $results->posts;
+        $this->assertEquals(count($results), 10);
+        foreach ($results as $result) {
+            $this->assertEquals($result->body->body, 'Hello world, I am a shout!');
+        }
     }
-    
+
+
+
 } 
