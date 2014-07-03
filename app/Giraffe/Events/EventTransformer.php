f4317fc7919db60622a09c709b41197f65d99f15
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-03 22:43:34 +0700

    Add PostTransformer, tweak Event & Shout transformers to fit same schema

--- app/Giraffe/Events/EventTransformer.php
+++ app/Giraffe/Events/EventTransformer.php
@@ -1,10 +1,13 @@
 <?php  namespace Giraffe\Events; 
+use Giraffe\Users\UserTransformer;
 use League\Fractal\TransformerAbstract;
 
 class EventTransformer extends TransformerAbstract
 {
     public function transform(EventModel $model)
     {
+        $author = (new UserTransformer())->transform($model->owner);
+
         return [
             'hash' => $model->hash,
             'name' => $model->name,
@@ -15,7 +18,10 @@ class EventTransformer extends TransformerAbstract
             'city' => $model->city,
             'state' => $model->state,
             'country' => $model->country,
-            'timestamp' => $model->timestamp
+            'timestamp' => $model->timestamp,
+            'links' => [
+                'owner' => $author
+            ]
         ];
     }
 } 
