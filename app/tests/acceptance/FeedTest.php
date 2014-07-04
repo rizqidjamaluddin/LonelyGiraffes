3641f08fc7dc2eece509aa1d905b598aa9900cfd
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-04 17:27:06 +0700

    Add pagination test; status red

--- app/tests/acceptance/FeedTest.php
+++ app/tests/acceptance/FeedTest.php
@@ -2,6 +2,8 @@
 
 class FeedTest extends AcceptanceCase
 {
+    protected $genericShoutBody = 'Hello world, I am a shout!';
+    protected $otherGenericShoutBody = "Hi, I'm a different kind of shout!";
 
     /**
      * @test
@@ -11,7 +13,7 @@ class FeedTest extends AcceptanceCase
         // add some test shouts
         $this->registerAndLoginAsMario();
         for ($i = 0; $i < 12; $i++) {
-            $this->call('POST', '/api/shouts', ['body' => 'Hello world, I am a shout!']);
+            $this->call('POST', '/api/shouts', ['body' => $this->genericShoutBody]);
         }
 
         // fetch them
@@ -20,8 +22,40 @@ class FeedTest extends AcceptanceCase
         $results = $results->posts;
         $this->assertEquals(count($results), 10);
         foreach ($results as $result) {
-            $this->assertEquals($result->body->body, 'Hello world, I am a shout!');
+            $this->assertEquals($result->body->body, $this->genericShoutBody);
         }
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_fetch_posts_by_the_cursor_of_the_bottom_most_post()
+    {
+        $this->registerAndLoginAsMario();
+
+        for ($i = 0; $i < 10; $i++) {
+            $this->call('POST', '/api/shouts', ['body' => $this->otherGenericShoutBody]);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $this->call('POST', '/api/shouts', ['body' => $this->genericShoutBody]);
+        }
+
+        $firstChunk = $this->toJson($this->call('GET', '/api/posts'));
+        $this->assertResponseOk();
+        $this->assertEquals($firstChunk->posts[0]->body->body, $this->otherGenericShoutBody);
+        $this->assertEquals($firstChunk->posts[9]->body->body, $this->otherGenericShoutBody);
+        $cursor = end($firstChunk->posts)->hash;
+
+        $nextChunk = $this->toJson($this->call('GET', '/api/posts', ['before' => $cursor]));
+        $this->assertResponseOk();
+        $this->assertEquals($firstChunk->posts[0]->body->body, $this->genericShoutBody);
+        $this->assertEquals($firstChunk->posts[9]->body->body, $this->genericShoutBody);
+        $cursor = end($firstChunk->posts)->hash;
+
+        $lastChunk = $this->toJson($this->call('GET', '/api/posts', ['before' => $cursor]));
+        $this->assertResponseOk();
+        $this->assertEquals(count($lastChunk->posts), 0);
     }
 
 
