693fbd2a87ca7ab7b5f00cd15c09bf91b937b121
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-05 19:40:56 +0700

    Add UserGeolocation test regarding disallowed location changes

--- app/tests/acceptance/geolocation/UserGeolocationTest.php
+++ app/tests/acceptance/geolocation/UserGeolocationTest.php
@@ -51,8 +51,22 @@ class UserGeolocationTest extends GeolocationCase
 
     }
 
+    /**
+     * @test
+     */
     public function a_user_cannot_change_another_users_location()
     {
+        $mario = $this->registerAndLoginAsMario();
+        $this->call('PUT', '/api/users/' . $mario->hash, $this->cities['nyc']);
+        $this->assertResponseOk();
+
+        $bowser = $this->registerAndLoginAsBowser();
+        $this->call('PUT', '/api/users/' . $mario->hash, $this->cities['manhattan']);
+
+        $check = $this->toJson($this->call('GET', '/api/users/' . $mario->hash))->users[0];
+        $this->assertEquals($check->city, $this->cities['nyc']['city']);
+        $this->assertEquals($check->state, $this->cities['nyc']['state']);
+        $this->assertEquals($check->country, $this->cities['nyc']['country']);
 
     }
 }
