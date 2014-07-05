3f78e05d0c67e3d7a4a5c119b60f880ffb1bb8ca
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-05 17:33:53 +0700

    Fix UserUpdateValidator to avoid overflows on city/state/country fields

--- app/Giraffe/Users/UserUpdateValidator.php
+++ app/Giraffe/Users/UserUpdateValidator.php
@@ -10,9 +10,12 @@ class UserUpdateValidator
     public function validate(array $data)
     {
         $validator = V::key('name', V::string()->length(0, 100), false)
-                      ->key('email', V::email()->length(0, 200),false)
+                      ->key('email', V::email()->length(0, 200), false)
                       ->key('password', V::string()->length(0, 200), false)
-                      ->key('gender', V::string()->in(['M', 'F', 'X']), false);
+                      ->key('gender', V::string()->in(['M', 'F', 'X']), false)
+                      ->key('city', V::string()->length(0, 150), false)
+                      ->key('state', V::string()->length(0, 150), false)
+                      ->key('country', V::string()->length(0, 150), false);
 
         try {
             $validator->assert($data);
