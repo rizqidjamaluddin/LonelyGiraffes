16d1173735099c9f8000ea8e83e1093b9a9a8921
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-05 17:34:04 +0700

    Add appropriate check to UserGeolocationTest

--- app/tests/acceptance/geolocation/UserGeolocationTest.php
+++ app/tests/acceptance/geolocation/UserGeolocationTest.php
@@ -11,5 +11,10 @@ class UserGeolocationTest extends GeolocationCase
         $this->call('PUT', '/api/users/' . $mario->hash, $this->cities['nyc']);
         $this->assertResponseOk();
 
+        $check = $this->toJson($this->call('GET', '/api/users/' . $mario->hash))->users[0];
+        $this->assertEquals($check->city, $this->cities['nyc']['city']);
+        $this->assertEquals($check->state, $this->cities['nyc']['state']);
+        $this->assertEquals($check->country, $this->cities['nyc']['country']);
+
     }
 }
