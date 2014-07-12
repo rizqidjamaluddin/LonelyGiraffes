4387eb0e215f327f206a15a573a6cdda1d287b30
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-12 08:14:55 +0700

    Implement Event service and repository

--- app/Giraffe/Events/EventRepository.php
+++ app/Giraffe/Events/EventRepository.php
@@ -1,11 +1,20 @@
 <?php  namespace Giraffe\Events; 
 
 use Giraffe\Common\EloquentRepository;
+use Giraffe\Geolocation\NearbySearchStrategies\TwoDegreeCellStrategy\TwoDegreeCellSearchableRepository;
 
-class EventRepository extends EloquentRepository
+class EventRepository extends EloquentRepository implements TwoDegreeCellSearchableRepository
 {
     public function __construct(EventModel $eventModel)
     {
         parent::__construct($eventModel);
     }
-} 
+
+    public function twoDegreeCellSearch(array $cell, $options = [])
+    {
+        $limit = array_key_exists('limit', $options) ? $options['limit'] : 10;
+        $skip = array_key_exists('skip', $options) ? $options['skip'] : 0;
+
+        return $this->model->whereIn('cell', $cell)->take($limit)->skip($skip)->get();
+    }
+}
