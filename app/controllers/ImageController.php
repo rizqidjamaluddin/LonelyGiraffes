783b50a5055f2a2c46aa1bf082435852bdb4ff32
Jon Petraglia <notbryant@gmail.com>
2014-07-22 16:30:12 -0600

    Routes & Controller for Images

---
+++ app/controllers/ImageController.php
@@ -0,0 +1,50 @@
+<?php
+
+use Giraffe\Common\Controller;
+use Giraffe\Images\ImageModel;
+use Giraffe\Images\ImageService;
+use Giraffe\Images\ImageTransformer;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class ImageController extends Controller {
+
+    /**
+     * @param Giraffe\Images\ImageService $imageService
+     */
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+        parent::__construct();
+    }
+
+    public function create()
+    {
+        if (!Input::hasFile('image') || !Input::has('user') || !input::has('type')) {
+            throw new BadRequestHttpException();
+        }
+
+        $image_type = Input::get('type');
+        $user_hash = Input::get('user');
+        $file = Input::file('image');
+
+        $model = $this->imageService->createImage($user_hash, $file, $image_type);
+        $model = $this->returnImageModel($model);
+        return $model;
+    }
+
+    public function delete($img_hash)
+    {
+        $model = $this->imageService->deleteImage($img_hash);
+        return $this->returnImageModel($model);
+    }
+
+    /**
+     * @param ImageModel $model
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function returnImageModel(ImageModel $model)
+    {
+        return $this->withItem($model, new ImageTransformer(), 'images');
+    }
+}
