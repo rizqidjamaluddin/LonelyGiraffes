1c9711e2fd511fb9c99a48d5d128b7531a47bc0e
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-07-27 23:10:31 +0700

    Rename ChatController to ChatroomController

---
+++ app/controllers/ChatroomController.php
@@ -0,0 +1,35 @@
+<?php
+
+use Giraffe\Chat\ChatroomTransformer;
+use Giraffe\Common\Controller;
+use Giraffe\Chat\ChatService;
+
+
+class ChatroomController extends Controller
+{
+    /**
+     * @var \Giraffe\Chat\ChatService
+     */
+
+    private $chatService;
+
+    public function __construct(ChatService $chatService)
+    {
+        $this->chatService = $chatService;
+        parent::__construct();
+    }
+
+    public function create()
+    {
+        $create = $this->chatService->createChatroom($this->gatekeeper->me());
+        return $this->withItem($create, new ChatroomTransformer(), 'chatrooms');
+    }
+
+    public function show($chatroom)
+    {
+
+    }
+
+
+
+}
