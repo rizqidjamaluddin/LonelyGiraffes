3b06e171c894f585ea780f59b11690045dd10c03
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-08-01 23:17:43 +0700

    Tests for user bios

---
+++ app/tests/acceptance/general/UserProfileTest.php
@@ -0,0 +1,85 @@
+<?php
+
+class UserProfileTest extends AcceptanceCase
+{
+    protected $simpleBio = 'Hey, this is my bio!';
+
+    /**
+     * @test
+     */
+    public function users_have_an_empty_profile_by_default_with_valid_keys_but_empty_values()
+    {
+        $mario = $this->registerAndLoginAsMario();
+        $fetch = $this->callJson('GET', "/api/users/{$mario->hash}/profile");
+        $this->assertResponseOk();
+
+        $this->assertEquals(count($fetch->profiles), 1);
+        $this->assertEquals($fetch->profiles[0]->bio, '');
+    }
+
+    /**
+     * @test
+     */
+    public function users_can_add_a_bio_and_anyone_can_see_them()
+    {
+        $mario = $this->registerAndLoginAsMario();
+        $addBio = $this->setSimpleBio($mario);
+        $this->assertResponseOk();
+
+        $this->validateSimpleBio($mario);
+        $luigi = $this->registerAndLoginAsLuigi();
+        $this->validateSimpleBio($mario);
+        $this->asGuest();
+        $this->validateSimpleBio($mario);
+    }
+
+    /**
+     * @test
+     */
+    public function user_bios_cannot_be_over_140_characters_long()
+    {
+        // put simple bio first
+        $mario = $this->registerAndLoginAsMario();
+        $this->setSimpleBio($mario);
+
+        $invalid = $this->callJson('POST', "/api/users/{$mario->hash}/profile", ['bio' => str_repeat('x', 200)]);
+        $this->assertResponseStatus(422);
+
+        $this->validateSimpleBio($mario);
+
+    }
+
+    /**
+     * @test
+     */
+    public function other_users_cannot_change_a_user_bio()
+    {
+        $mario = $this->registerAndLoginAsMario();
+        $simple = $this->setSimpleBio($mario);
+
+        $bowser = $this->registerAndLoginAsBowser();
+        $invalid =  $this->callJson('POST', "/api/users/{$mario->hash}/profile", ['bio' => 'Evil bio!']);
+        $this->assertResponseStatus(401);
+        $this->validateSimpleBio($mario);
+    }
+
+    /**
+     * @param $mario
+     */
+    protected function validateSimpleBio($mario)
+    {
+        $fetch = $this->callJson('GET', "/api/users/{$mario->hash}/profile")->profiles[0];
+        $this->assertResponseOk();
+        $this->assertEquals($fetch->bio, $this->simpleBio);
+    }
+
+    /**
+     * @param $mario
+     * @return mixed
+     */
+    protected function setSimpleBio($mario)
+    {
+        return $this->callJson('POST', "/api/users/{$mario->hash}/profile", ['bio' => $this->simpleBio]);
+    }
+
+} 
