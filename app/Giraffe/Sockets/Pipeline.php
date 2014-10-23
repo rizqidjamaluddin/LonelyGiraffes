037461f1f4ea9bd06321027e22bf534f5a6552eb
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-10-23 13:31:55 +0700

    Apply lazy loading to Pipeline

--- app/Giraffe/Sockets/Pipeline.php
+++ app/Giraffe/Sockets/Pipeline.php
@@ -1,5 +1,6 @@
 <?php  namespace Giraffe\Sockets;
 
+use Giraffe\Common\ConfigurationException;
 use Illuminate\Redis\Database;
 use Predis\Client;
 
@@ -21,25 +22,37 @@ class Pipeline
      */
     protected $servers;
 
+    /**
+     * @var bool
+     */
+    protected $connected = false;
+
     protected $channel = 'lg-bridge:pipeline';
 
-    public function __construct(Database $d)
+    public function __construct()
     {
+    }
+
+    public function issue($endpoint)
+    {
+        $this->assertConnected();
+        foreach ($this->servers as $server) {
+            $server->publish($this->channel, json_encode(['endpoint' => $endpoint]));
+        }
+    }
+
+    protected function assertConnected()
+    {
+        if ($this->connected) return;
         $connections = \Config::get('sockets.broadcast');
         if (isset($connections) && count($connections) > 0 ) {
             foreach ($connections as $connection) {
                 $this->servers[] = new Client($connection);
             }
         } else {
-            $this->servers = $d->connection();
+            throw new ConfigurationException("No broadcast servers defined for sockets (Config sockets.broadcast)");
         }
-    }
-
-    public function issue($endpoint)
-    {
-        foreach ($this->servers as $server) {
-            $server->publish($this->channel, json_encode(['endpoint' => $endpoint]));
-        }
+        $this->connected = true;
     }
 
     public function serializeForBridge($data)
