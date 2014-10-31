c7f5b7f4737850d1d45e5dabb470b7139c7450d5
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-10-31 21:49:46 +0700

    Implement socket authenticator

--- app/Giraffe/Sockets/Authenticator.php
+++ app/Giraffe/Sockets/Authenticator.php
@@ -2,6 +2,7 @@
 
 use Giraffe\Common\NotFoundModelException;
 use Giraffe\Users\UserRepository;
+use League\OAuth2\Server\Storage\SessionInterface;
 
 class Authenticator
 {
@@ -11,21 +12,44 @@ class Authenticator
      */
     private $userRepository;
 
-    public function __construct(UserRepository $userRepository)
+    /**
+     * @var \League\OAuth2\Server\Storage\SessionInterface
+     */
+    private $sessionManager;
+
+    public function __construct(UserRepository $userRepository, SessionInterface $sessionManager)
     {
         $this->userRepository = $userRepository;
+        $this->sessionManager = $sessionManager;
     }
 
-    public function attempt($hash, $token)
+    /**
+     * @param $token
+     *
+     * @return bool|\Giraffe\Users\UserModel
+     */
+    public function attempt($token)
     {
+
+        $validate = $this->sessionManager->validateAccessToken($token);
+
+        if (!$validate) {
+            return false;
+        }
+
+        $type = $validate['owner_type'];
+        $userId = $validate['owner_id'];
+
+        if ($type != 'user') {
+            return false;
+        }
+
         try {
-            $user = $this->userRepository->getByHash($hash);
+            $user = $this->userRepository->getById($userId);
         } catch (NotFoundModelException $e) {
             return false;
         }
 
-        
-
-        return true;
+        return $user;
     }
 } 
