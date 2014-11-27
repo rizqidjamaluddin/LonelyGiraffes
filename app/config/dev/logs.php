0bfd4d10245c571f76627debf220b6f5221a018e
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-11-27 09:28:13 +0700

    Add GelfMessageFormatter to logger

--- app/config/dev/logs.php
+++ app/config/dev/logs.php
@@ -16,6 +16,7 @@ return array(
         $logger = new Logger('lg');
 
         $graylogHandler = new GelfHandler(new Publisher(new UdpTransport("172.21.0.3", 12201)), Logger::INFO);
+        $graylogHandler->setFormatter(new \Monolog\Formatter\GelfMessageFormatter());
         $flowdockHandler = new FlowdockChatHandler('014067cfe257dc4572f903b6b440f7ed', Logger::INFO);
         $flowdockHandler->setFormatter(new \Giraffe\Logging\FlowdockChatFormatter('014067cfe257dc4572f903b6b440f7ed'));
         $essentialHandler = new FingersCrossedHandler(new StreamHandler(storage_path() . '/essential/lg.log'), new ErrorLevelActivationStrategy(Logger::ERROR));
