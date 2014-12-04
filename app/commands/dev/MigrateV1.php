f5c9ee9fd6f32c575f484d7fb2fb27ab240f1f43
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-12-04 09:23:57 +0700

    Add CI password dehasher

--- app/commands/dev/MigrateV1.php
+++ app/commands/dev/MigrateV1.php
@@ -76,14 +76,14 @@ class MigrateV1 extends Command
             $user = [
                 'name' => $name,
                 'email' => $user['email'],
-                'password' => $user['password'],
+                'password' => $this->handleLegacyCodeIgniterPassword($user['password']),
                 'hash' => Str::random(32),
                 'role' => 'member',
                 'created_at' => $user['joined'],
                 'updated_at' => time(),
             ];
 
-
+            // enter location data
             if ($user['zip_code']) {
                 $locations = $locationService->search($user['zip_code'], 1);
                 if (count($locations) != 0) {
@@ -96,9 +96,58 @@ class MigrateV1 extends Command
                 }
             }
 
-            $userRepository->create($user);
-            $this->info('Created user for ' . $user['email'] . '.');
+            // handle avatars
+
+            try {
+                $userRepository->create($user);
+                $this->info('Created user for ' . $user['email'] . '.');
+            } catch (Exception $e) {
+                $this->error('Unable to create user for ' . $user['email'] . ': ' . $e->getMessage());
+            }
         }
+
+        return true;
+    }
+
+    /**
+     * I made 20 people on #laravel-offtopic go WTF at the CI encrypt library. SCORE.
+     *
+     * @param $password
+     */
+    protected function handleLegacyCodeIgniterPassword($password)
+    {
+        if ($password[0] == '$') {
+            return $password;
+        }
+        $key = '2a7af90c898ce26ea993398d966615bd'; // md5 of 'DXTDO4O3pxLTDo53LesTbtYsFXFFW2oV', it was CI's idea;
+
+        $data = $this->_remove_cipher_noise($password, $key);
+        $init_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+        $init_vect = substr($data, 0, $init_size);
+        $data = substr($data, $init_size);
+        return Hash::make(rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, $init_vect), "\0"));
+    }
+
+    function _remove_cipher_noise($data, $key) {
+        $keyhash = sha1($key);
+        $keylen = strlen($keyhash);
+        $str = '';
+
+        for ($i = 0, $j = 0, $len = strlen($data); $i < $len; ++$i, ++$j) {
+            if ($j >= $keylen) {
+                $j = 0;
+            }
+
+            $temp = ord($data[$i]) - ord($keyhash[$j]);
+
+            if ($temp < 0) {
+                $temp = $temp + 256;
+            }
+
+            $str .= chr($temp);
+        }
+
+        return $str;
     }
 
     /**
