95463b78f9f0d8b1098d12f32bb88ec921fe9c4e
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2014-12-04 09:34:43 +0700

    Update image config file with filesystem info

--- app/config/images.php
+++ app/config/images.php
@@ -1,4 +1,8 @@
 <?php
+use Aws\S3\S3Client;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\AwsS3;
+
 return [
     /*
      * Path where files will be stored before they are sent up to the permanent
@@ -10,14 +14,18 @@ return [
      * Permanent storage medium for avatars. Return a League\Flysystem\Filesystem instance.
      * https://github.com/thephpleague/flysystem
      */
-    'medium' => function() {
+    'medium' => function () {
+        $accessKey = 'AKIAI4HJVQ3DFNICQVAA';
+        $secretKey = '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';
+        $bucket = 'lonelygiraffes.com';
 
+        $client = S3Client::factory(['key' => $accessKey,'secret' => $secretKey]);
+        $filesystem = new Filesystem(new AwsS3($client, $bucket, 'production'));
+        return $filesystem;
     },
 
     /**
      * Prefix to use when serving image URLs to clients.
      */
-    'url_prefix' => 'http://i.lonelygiraffes.com/'
-
-
+    'url_prefix'   => 'http://lonelygiraffes.com.s3-website-us-west-2.amazonaws.com'
 ];
